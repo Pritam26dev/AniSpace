c912e9792ce40df26560ad956db226807fe5e5c2
Pritam Debnath <24155487@kiit.ac.in>
2026-06-05 23:48:43 +0530

    watchlist page

--- anispace/src/app/watchlist/page.jsx
+++ anispace/src/app/watchlist/page.jsx
@@ -3,21 +3,38 @@ import {useEffect,useState} from "react";
 
 export default function WatchlistPage(){
     const [watchlist,setWatchlist]=useState([]);
+    console.log(watchlist);
 
-    useEffect(()=>{
-        const savedWatchlist=localStorage.getItem("watchlist");
+    useEffect(() => {
+  const savedWatchlist = localStorage.getItem("watchlist");
 
-        if(savedWatchlist){
-            setWatchlist(
-                JSON.parse(savedWatchlist)
-            )
-        }
-    })
+  if (savedWatchlist) {
+    setWatchlist(JSON.parse(savedWatchlist));
+  }
+}, []);
     return(
         <main>
             <h1>My WatchList</h1>
 
-            <p className="text-gray-400 mt-4"> No anime added yet</p>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-6">
+                {watchlist.length === 0?( 
+                <p>
+                    No Anime Added yet
+                </p>
+            ):(
+                watchlist.map((anime)=>(
+                    <div key={anime.id} 
+                    className="bg-[#14141c] rounded-xl overflow-hidden">
+                        <div className="p-4">
+                            <h2 className="font-semibold">
+                                {anime.title}
+                            </h2>
+                        </div>
+                    </div>
+                ))
+            
+            )}
+            </div>
         </main>
     )
 }
